1cc6d6414e8dd46b797cda9c3b8a8d4508d97bf0
Kazuhito Higashioka <kmhigashioka@gmail.com>
2021-02-11 01:56:16 +0800

    Add UserDialog component

--- src/containers/UsersScreen/UsersList.jsx
+++ src/containers/UsersScreen/UsersList.jsx
@@ -1,4 +1,22 @@
+import * as React from 'react';
+import UserDialog from './UserDialog';
+
 function UsersList({ users, activePageNumber, handlePaginate, totalPages }) {
+  const [isUserDialogOpen, setIsUserDialogOpen] = React.useState(false);
+  const [selectedUserId, setSelectedUserId] = React.useState(null);
+  const selectedUser = users.find(u => u.id === selectedUserId);
+
+  function handleUserClick(userId) {
+    return function () {
+      setSelectedUserId(userId);
+      setIsUserDialogOpen(true);
+    }
+  }
+
+  function handleDismiss() {
+    setIsUserDialogOpen(false);
+  }
+
   return (
     <div>
       <table>
@@ -12,7 +30,7 @@ function UsersList({ users, activePageNumber, handlePaginate, totalPages }) {
         </thead>
         <tbody>
           {users.map(user => (
-            <tr key={user.id}>
+            <tr key={user.id} onClick={handleUserClick(user.id)}>
               <td>{user.id}</td>
               <td>{user.first_name}</td>
               <td>{user.last_name}</td>
@@ -27,12 +45,15 @@ function UsersList({ users, activePageNumber, handlePaginate, totalPages }) {
         {Array.from({ length: totalPages }).map((_, index) => {
           const pageNumber = index + 1;
           return (
-            <button type="button" onClick={handlePaginate(pageNumber)}>{pageNumber}</button>
+            <button type="button" onClick={handlePaginate(pageNumber)} key={pageNumber}>{pageNumber}</button>
           );
         })}
         <button disabled={activePageNumber === totalPages} type="button" onClick={handlePaginate(activePageNumber + 1)}>{'>'}</button>
         <button disabled={activePageNumber === totalPages} type="button" onClick={handlePaginate(totalPages)}>{'>>'}</button>
       </div>
+      {selectedUser
+        ? <UserDialog isOpen={isUserDialogOpen} onDismiss={handleDismiss} user={selectedUser} />
+        : null}
     </div>
   );
 }
